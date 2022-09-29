3e5f515722845dfaf2e9c5296dd01902e731eb3f
jikwan <jikwan0327@gmail.com>
2022-09-29 11:45:59 +0900

    Nomad Next

--- next-intro/pages/index.js
+++ next-intro/pages/index.js
@@ -1,21 +1,26 @@
 import Head from "next/head";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
 export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push(`/movies/${title}/${id}`);
+  };
   return (
     <div className="container">
       <Seo title="Home"></Seo>
       {results?.map((movie) => (
-        <Link key={movie.id}>
-          <a>
-            <div className="movie" key={movie.id}>
-              <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-              <h4>{movie?.original_title}</h4>
-            </div>
-          </a>
-        </Link>
+        <div onClick={() => onClick(movie.id, movie.original_title)} className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <h4>
+            <Link href={`/movies/${movie.original_title}/${movie.id}`}>
+              <a>{movie?.original_title}</a>
+            </Link>
+          </h4>
+        </div>
       ))}
       <style jsx>{`
         .container {
