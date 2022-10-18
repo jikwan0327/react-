f534c5beca5b0eea13ec0aee368370ac43cf62a3
jikwan <jikwan0327@gmail.com>
2022-10-18 23:09:37 +0900

    middleware & reduxdevtools

--- node-bird/store/configureStore.js
+++ node-bird/store/configureStore.js
@@ -1,17 +1,18 @@
 // next-redux-wrapper 기본 세팅
 
 import { createWrapper } from "next-redux-wrapper";
-import { applyMiddleware, compose, legacy_createStore as createStore } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import reducer from "../reducers";
 
 const configureStore = () => {
-  const enhancer = process.env.NODE_ENV === "production" ? compose(applyMiddleware([])) : compose(applyMiddleware([]));
+  const middlewares = [];
+  const enhancer =
+    process.env.NODE_ENV === "production"
+      ? compose(applyMiddleware(...middlewares))
+      : composeWithDevTools(applyMiddleware(...middlewares)); //배포를 하는 경우 devtool을 연결하지 않고 개발할때는 devtool을 연결
 
   const store = createStore(reducer, enhancer);
-  store.dispatch({
-    type: "LOG_IN",
-    data: "jungjik",
-  });
   return store;
 };
 
